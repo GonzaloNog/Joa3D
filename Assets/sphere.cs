9468edbe38f390d740add7f36e24d62e1913ec24
JoaquinSuez <joaquin233suez@gmail.com>
2023-07-21 17:14:34 -0300

    tarea

--- Assets/sphere.cs
+++ Assets/sphere.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class sphere : MonoBehaviour
 {
+    public Material material;
+    public MeshRenderer meshRenderer;
+    public GameObject muro;
+
+    //audio
+    public AudioSource col;
+    public AudioSource pin;
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject == muro) 
+        {
+            Color color = new(Random.value, Random.value, Random.value);
+            meshRenderer.material.color = color;
+        }
+
+        //audio
+        if (collision.gameObject.tag == "cilindro")
+        {
+            Debug.Log("cilindro");
+            col.Play();
+        }
+        if (collision.gameObject.tag == "suelo")
+        {
+            Debug.Log("suelo");
+            pin.Play();
+        }
     }
     private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("hola");
     }
 }
